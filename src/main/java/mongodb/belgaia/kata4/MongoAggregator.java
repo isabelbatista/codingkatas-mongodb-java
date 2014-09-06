e0333aa90983cf7d52bcf5cb432f08f7e0fb08f3
isabel <isabelbatista@gmx.de>
2014-09-06 22:52:19 +0200

    Kata 4: Added testcases and implemented average calculation (not finished, yet).

--- src/main/java/mongodb/belgaia/kata4/MongoAggregator.java
+++ src/main/java/mongodb/belgaia/kata4/MongoAggregator.java
@@ -1,12 +1,17 @@
 package mongodb.belgaia.kata4;
 
 import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.mongodb.AggregationOutput;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import com.mongodb.DBRef;
 import com.mongodb.Mongo;
 
 public class MongoAggregator {
@@ -14,12 +19,18 @@ public class MongoAggregator {
 	private static final String DATABASE_NAME = "mobilerobotics";
 	private Mongo client;
 	private DB database;
+	
+	private DBCollection roboFlyCollection;
+	private DBCollection measurementCollection;
 
 	public MongoAggregator() {
 		
 		try {
 			client = new Mongo("localhost", 27017);
 			database = client.getDB(DATABASE_NAME);
+			
+			initDatabaseElements();
+			
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -31,28 +42,72 @@ public class MongoAggregator {
 		try {
 			client = new Mongo("localhost", 27017);
 			database = client.getDB(databaseName);
+			
+			initDatabaseElements();
+			
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public void saveRoboflies(List<DBObject> roboFlies) {
-				
-		DBCollection collection = database.getCollection("roboflies");
-		
-		collection.insert(roboFlies);		
+	public void saveRoboflies(List<DBObject> roboFlies) {		
+		roboFlyCollection.insert(roboFlies);		
 	}
 	
-	public List<RoboFly> getRoboFliesWithDifferingVolume() {
+	public void saveMeasurements(List<DBObject> measurements) {
+		measurementCollection.insert(measurements);
+	}
+	
+	public RoboFly findRoboFlyWithWrongSoundIntensity() {
+		
+		// group by roboFly // average of soundIntensity
+		calcAverage();
+		return null;
+	}
+	
+	private void calcAverage() {
+		
+		DBObject average = addAverage();
+		
+		AggregationOutput output = measurementCollection.aggregate(average);
+		for(DBObject result : output.results()) {
+			System.out.println("Output: " + result);
+		}
+
+	}
+	
+	private DBObject addAverage() {
+		
+		// calculates the average of documents of each robofly separately
+//		DBObject groupFields = new BasicDBObject( "_id", "$roboFlyID");
+//		groupFields.put("average", new BasicDBObject( "$avg", "$soundIntensity"));
+		
+		// calculates the average of all documents of the collection
+		DBObject average = new BasicDBObject("_id", "$roboFlyID").append("average", new BasicDBObject("$avg", "$soundIntensity"));
+		return new BasicDBObject("$group", average);
+	}
+		
+	private DBObject addProjection() {
+		
+		DBObject fields = new BasicDBObject("roboFlyId", 1);
+		fields.put("soundIntensity", 1);
+		return new BasicDBObject("$project", fields);
+	}
+	
+	public void createDocumentReference(String roboFlyReferenceId, DBObject measurement) {
+					
+		DBRef docReference = new DBRef(database, "roboflies", roboFlyReferenceId);
+		measurement.put("robofly", docReference);
 		
 		DBCollection collection = database.getCollection("measurements");
+		collection.save(measurement);
+	}
+	
+	private void initDatabaseElements() {
 		
-//		DBObject average = new BasicDBObject("$avg", new BasicDBObject("SoundIntensitiy (in decibel)"));
-//		
-//		DBObject match = new BasicDBObject("$match", new BasicDBObject("SoundIntensity", 0));
-		
-		return null;
+		roboFlyCollection = database.getCollection("roboflies");
+		measurementCollection = database.getCollection("measurements");
 	}
 
 }
--- src/test/java/mongodb/belgaia/kata4/TestMongoAggregator.java
+++ src/test/java/mongodb/belgaia/kata4/TestMongoAggregator.java
@@ -1,6 +1,7 @@
 package mongodb.belgaia.kata4;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Assert;
@@ -25,12 +26,15 @@ public class TestMongoAggregator {
 	}
 	
 	@Test
-	public void testGetRoboFliesWithDifferingVolume() {
+	public void shouldReturnRoboFlyWhereSoundIntensityIsTooLow() {
 		
-		List<RoboFly> toBeCalibratedRoboFlies = aggregator.getRoboFliesWithDifferingVolume();
+		RoboFly roboFlyToBeCalibrated = aggregator.findRoboFlyWithWrongSoundIntensity();
 		
-		Assert.assertNotNull(toBeCalibratedRoboFlies);
-		Assert.assertEquals(1, toBeCalibratedRoboFlies.size());
+		
+		
+		
+		String expectedRoboFlyId = "RoboFly_ID_2";
+		
 		
 	}
 	
@@ -41,22 +45,16 @@ public class TestMongoAggregator {
 		DBObject roboFly1 = new BasicDBObject("name", "Calliphora")
 										.append("_id", "RoboFly_ID_1")
 										.append("constructionYear", 2014)
-										.append("size", 2)
-										.append("serviceTime", 60)
 										.append("status", "OK");
 		
 		DBObject roboFly2 = new BasicDBObject("name", "Lucilia")
 										.append("_id", "RoboFly_ID_2")
 										.append("constructionYear", 2014)
-										.append("size", 2)
-										.append("serviceTime", 60)
 										.append("status", "OK");
 
 		DBObject roboFly3 = new BasicDBObject("name", "Onesia")
-										.append("_id", "RoboFly_ID_2")
+										.append("_id", "RoboFly_ID_3")
 										.append("constructionYear", 2014)
-										.append("size", 2)
-										.append("serviceTime", 60)
 										.append("status", "OK");
 		
 		roboFlies.add(roboFly1);
@@ -69,8 +67,47 @@ public class TestMongoAggregator {
 
 	private void createInitialMeasurements() {
 		
-		System.out.println("To be implemented.");
-
+		List<DBObject> measurements = new ArrayList<DBObject>();
 		
+		DBObject measurement1 = new BasicDBObject("_id", "measurement_average_1")
+					.append("timestamp", System.currentTimeMillis())
+					.append("roboFlyID", "RoboFly_ID_1")
+					.append("humidity", 20.01)
+					.append("airPressure", 1013.25)
+					.append("luminosity", 600)
+					.append("soundIntensity", 55)
+					.append("temperature", 17)
+					.append("co2Content", 0.04);
+		
+		DBObject measurement2 = new BasicDBObject("_id", "measurement_average_2")
+					.append("timestamp", System.currentTimeMillis())
+					.append("roboFlyID", "RoboFly_ID_2")
+					.append("humidity", 20.01)
+					.append("airPressure", 1013.25)
+					.append("luminosity", 600)
+					.append("soundIntensity", 0)		// shows difference -- no value
+					.append("temperature", 17)
+					.append("co2Content", 2.17);		// shows difference -- very high value
+			
+		DBObject measurement3 = new BasicDBObject("_id", "measurement_average_3")
+					.append("timestamp", System.currentTimeMillis())
+					.append("roboFlyID", "RoboFly_ID_2")
+					.append("humidity", 20.01)
+					.append("airPressure", 1013.25)
+					.append("luminosity", 600)
+					.append("soundIntensity", 57)
+					.append("temperature", 17)
+					.append("co2Content", 0.5);
+	
+		
+		aggregator.createDocumentReference("RoboFly_ID_1", measurement1);
+		aggregator.createDocumentReference("RoboFly_ID_2", measurement2);
+		aggregator.createDocumentReference("RoboFly_ID_2", measurement3);
+		
+		measurements.add(measurement1);
+		measurements.add(measurement2);
+		measurements.add(measurement3);
+		
+		aggregator.saveMeasurements(measurements);
 	}
 }
